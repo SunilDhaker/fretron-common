e94b9c6b9315f456474cff20d2a068041c5da909
RohitPalod <palodrht@gmail.com>
2018-01-19 15:48:17 +0530

    Shipmentdash avsc change ,, httprequest helper class

---
+++ src/main/java/com/fretron/HttpRequestHepler.java
@@ -0,0 +1,49 @@
+package com.fretron;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HttpRequestHepler {
+
+
+    public static HttpEntity makePostRequest(String uri, Object requestObject , String authToken ){
+        HttpResponse response = null;
+        HttpEntity entity = null;
+        final HttpParams httpParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+
+        DefaultHttpClient httpClient = new DefaultHttpClient(httpParams);
+        HttpPost post = new HttpPost(uri);
+        post.setHeader("Content-Type", "application/json");
+        if(authToken!=null){
+            post.setHeader("Authorization",authToken);
+        }
+        StringEntity se = null;
+        try {
+            se = new StringEntity(requestObject.toString());
+            post.setEntity(se);
+
+            try {
+                response = httpClient.execute(post);
+                entity = response.getEntity();
+            } catch (IOException e) {
+                Logger.getGlobal().log(Level.WARNING,"Connection expection: "+e.getMessage());
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return entity;
+    }
+}
