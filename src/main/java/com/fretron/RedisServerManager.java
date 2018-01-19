5bb1cb27e81dd281a4b61661e6210835630dbf63
RohitPalod <palodrht@gmail.com>
2018-01-19 13:19:33 +0530

    Try catch in redis manager

--- src/main/java/com/fretron/RedisServerManager.java
+++ src/main/java/com/fretron/RedisServerManager.java
@@ -3,6 +3,8 @@ package com.fretron;
 import redis.clients.jedis.Jedis;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by PalodRohit on 11/14/2017.
@@ -13,35 +15,56 @@ public class RedisServerManager {
     static Jedis jedis ;
     private String host ;
     public RedisServerManager(String host)  {
-        if(jedis == null ){
-            this.host = host;
-            jedis = new Jedis(host);
-            System.out.println("Connection to Redis server sucessfully "+jedis.ping());
+        try {
+            if(jedis == null ){
+                this.host = host;
+                jedis = new Jedis(host);
+                System.out.println("Connection to Redis server sucessfully "+jedis.ping());
+            }
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.WARNING,"Error in connecting with redis "+e.getMessage());
         }
     }
 
 
     public void saveInfo(String key, String  value){
-        jedis.set(key,value);
+        try {
+            jedis.set(key,value);
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.WARNING,"Error :: "+e.getMessage());
+        }
     }
 
     public String getValueForKey(String key){
-        List<String> list = jedis.mget(key);
-        if (list.size() > 0) {
-            return list.get(0);
+        try {
+            List<String> list = jedis.mget(key);
+            if (list.size() > 0) {
+                return list.get(0);
+            }
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.WARNING,"Error :: "+e.getMessage());
         }
 
         return null;
     }
 
     public void deleteKey(String key){
-        jedis.del(key);
+        try {
+            jedis.del(key);
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.WARNING,"Error :: "+e.getMessage());
+        }
     }
 
 
 
     public boolean isConnected(){
-        boolean isConnected =  jedis.isConnected();
+        boolean isConnected = false;
+        try {
+            isConnected = jedis.isConnected();
+        } catch (Exception e) {
+            Logger.getGlobal().log(Level.WARNING,"Error :: "+e.getMessage());
+        }
 
 //        if(isConnected == false){
 //            System.out.println("not connected to redis,, trying to reconnect");
@@ -60,7 +83,11 @@ public class RedisServerManager {
     public static boolean isRedisServerConnected(){
         boolean isConnected = false;
         if(jedis!=null){
-            isConnected = jedis.isConnected();
+            try {
+                isConnected = jedis.isConnected();
+            } catch (Exception e) {
+                Logger.getGlobal().log(Level.WARNING,"Error :: "+e.getMessage());
+            }
         }
         return isConnected;
     }
