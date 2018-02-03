b194897623dff1df5ca4138d3960ba6987f483a3
Akash <akashkumardutta2@gmail.com>
2018-02-03 18:30:48 +0530

    token manager code added

--- src/main/java/com/fretron/Constant/Constants.java
+++ src/main/java/com/fretron/Constant/Constants.java
@@ -438,7 +438,7 @@ public class Constants {
     //**********************Token Manager ******************************************************************************/
     public final static String KEY_TOKEN_MANAGER_APP_ID = "token.manager.app.id";
     public final static String KEY_QUEUE_TOPIC="queue.topic";
-    public final static String KEY_QUEUE_TOKNE_TOPIC="queue.token.topic";
+    public final static String KEY_QUEUE_TOKEN_TOPIC="queue.token.topic";
 
     public final static String QUEUE_IP = "queue.ip";
     public final static String QUEUE_PORT = "queue.port";
