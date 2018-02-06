038931b11d413b0795b6ef0ae10cb9ebff81ca39
Mohit <sahumohit00786@gmail.com>
2018-02-06 14:53:04 +0530

    remove unwanted imports

--- src/main/java/com/fretron/utils/compressedPolylineUtils/CompressPolyline.java
+++ src/main/java/com/fretron/utils/compressedPolylineUtils/CompressPolyline.java
@@ -1,23 +1,21 @@
 package com.fretron.utils.compressedPolylineUtils;
 
 
-
 import com.fretron.Constant.Constants;
 import com.fretron.Model.PointAtTime;
 import com.fretron.Model.TimeAwarePolyline;
 import com.fretron.utils.compressedPolylineUtils.reducer.SeriesReducer;
-import com.fretron.utils.PolylineUtils.Point;
 import com.fretron.utils.PolylineUtils.PolylineDecoder;
 
 
-import javax.json.JsonObject;
+
 
 import static com.fretron.utils.compressedPolylineUtils.reducer.PointImpl.p;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+
 
 import static com.fretron.utils.PolylineUtils.OnlinePolylineEncoder.*;
 
