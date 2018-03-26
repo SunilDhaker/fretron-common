68eabe8891a41e2d336fe7561ff3726c42cfc47f
Mohit Sahu <sahumohit00786@gmail.com>
2018-03-26 19:18:23 +0530

    add constant for alleypoint tpi integration

--- src/main/java/com/fretron/Constant/Constants.java
+++ src/main/java/com/fretron/Constant/Constants.java
@@ -473,6 +473,7 @@ public class Constants {
     public  final static String KEY_TF_TRACK_VEHICLE_API_URL = "tatafleetman.track.vehicle";
     public  final static String KEY_TRACK_YOUR_FLEET_API_URL = "track.your.fleet.api.url";
     public  final static String KEY_TRUCK_SUVIDHA_API_URL = "truck.suvidha.api.url";
+    public  final static String KEY_ALLEY_TRACK_POINT_API_URL = "alley.track.point.api.url";
 
 
     public final static String KEY_JOB_SCHEDULE_TIME = "integration.job.schedule.time";
