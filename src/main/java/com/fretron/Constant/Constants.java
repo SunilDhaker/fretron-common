f83651e8477a3a2aa56d60bd20f0b4185bf6c90d
rohitpalod <palodrht@gmail.com>
2018-06-09 13:03:43 +0530

    Some constant for tpi

--- src/main/java/com/fretron/Constant/Constants.java
+++ src/main/java/com/fretron/Constant/Constants.java
@@ -501,6 +501,7 @@ public class Constants {
     public static final String KEY_JOB_RAW_RESPONSE_TOPIC = "integration.job.raw.response.topic";
     public static final String KEY_INTEGRATION_MANAGER_IP = "integration.manager.restservices.ip";
     public static final String KEY_INTEGRATION_MANAGER_PORT = "integration.manager.restservices.port";
+    public static final String KEY_TPI_SINKTASK_APP_ID = "tpi.sink.task.manager.app.id";
 
 
 
