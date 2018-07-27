046497f6d61955717be9d326bf0a32d9c9ffbbae
UMESH <umeshy790@gmail.com>
2018-07-27 18:14:32 +0530

    constants updated for elastic serach

--- src/main/java/com/fretron/Constant/Constants.java
+++ src/main/java/com/fretron/Constant/Constants.java
@@ -670,6 +670,10 @@ public class Constants {
     public static String ELASTIC_SEARCH_PORT = "elastic.search.port";
     public static String ELASTIC_SEARCH_APP_ID = "elastic.search.application.id";
 
+    public static String PLACE_RESYNC_TOPIC = "places.resync.topic";
+    public static String ELASTIC_SEARCH_MANAGER_IP = "elastic.serach.manager.ip";
+    public static String ELASTIC_SEARCH_MANAGER_PORT = "elastic.serach.manager.port";
+
     private Constants(){
 
     }
