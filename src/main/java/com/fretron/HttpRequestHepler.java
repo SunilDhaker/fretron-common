76a16b48212bb7de85bc4dce78c4b98f46b8b9fc
SahuMohit <sahumohit00786@gmail.com>
2018-10-05 16:40:48 +0530

    add timeout in create topic api

--- src/main/java/com/fretron/HttpRequestHepler.java
+++ src/main/java/com/fretron/HttpRequestHepler.java
@@ -17,75 +17,77 @@ import java.util.HashMap;
  */
 public class HttpRequestHepler {
 
-  private Client client;
+    private Client client;
 
-  public HttpRequestHepler(Client client) {
+    public HttpRequestHepler(Client client) {
 
-    this.client = client;
-  }
+        this.client = client;
+    }
 
-  public static HttpRequestHepler getInstance() {
-    Client client = ClientBuilder.newBuilder().build();
-    return new HttpRequestHepler(client);
-  }
+    public static HttpRequestHepler getInstance() {
+        Client client = ClientBuilder.newBuilder().build();
+        return new HttpRequestHepler(client);
+    }
 
-  public Response makeGetRequest(String uri, String authToken) {
-    return client
-        .target(uri)
-        .request(MediaType.APPLICATION_JSON)
-        .header("Authorization", "Bearer " + authToken)
-        .get();
-  }
+    public Response makeGetRequest(String uri, String authToken) {
+        return client
+                .target(uri)
+                .request(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + authToken)
+                .get();
+    }
 
-  public Response makeGetRequest(String uri, String authToken, int timeout) {
-    return client
-        .property(ClientProperties.CONNECT_TIMEOUT, timeout)
-        .target(uri)
-        .request(MediaType.APPLICATION_JSON)
-        .header("Authorization", "Bearer " + authToken)
-        .get();
-  }
+    public Response makeGetRequest(String uri, String authToken, int timeout) {
+        return client
+                .property(ClientProperties.CONNECT_TIMEOUT, timeout)
+                .target(uri)
+                .request(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + authToken)
+                .get();
+    }
 
-  public Response makePostRequest(String uri, Object requestObject, String authToken) {
+    public Response makePostRequest(String uri, Object requestObject, String authToken) {
 
-    HashMap<String, String> headermap = new HashMap<String, String>();
-    headermap.put("Authorization", "Bearer " + authToken);
-    return makePostRequest(uri, requestObject, headermap);
-  }
+        HashMap<String, String> headermap = new HashMap<String, String>();
+        headermap.put("Authorization", "Bearer " + authToken);
+        return makePostRequest(uri, requestObject, headermap);
+    }
 
-  public String makePostRequestAndGetData(String uri, Object requestObject, String authToken) {
+    public String makePostRequestAndGetData(String uri, Object requestObject, String authToken) {
 
-    Response response = makePostRequest(uri, requestObject, authToken);
-    return response.readEntity(String.class);
-  }
+        Response response = makePostRequest(uri, requestObject, authToken);
+        return response.readEntity(String.class);
+    }
 
-  public String makeGetRequestAndGetData(String uri, String authToken, int timeout) {
+    public String makeGetRequestAndGetData(String uri, String authToken, int timeout) {
 
-    Response response = makeGetRequest(uri, authToken, timeout);
-    return response.readEntity(String.class);
-  }
+        Response response = makeGetRequest(uri, authToken, timeout);
+        return response.readEntity(String.class);
+    }
 
-  public Response makePostRequest(String uri, Object requestObject,
-      HashMap<String, String> headers) {
+    public Response makePostRequest(String uri, Object requestObject,
+                                    HashMap<String, String> headers) {
 
-    MultivaluedMap<String, Object> headerMap = new MultivaluedHashMap<>(headers);
-    return client
-        .target(uri)
-        .request(MediaType.APPLICATION_JSON)
-        .headers(headerMap)
-        .post(Entity.entity(requestObject.toString(), MediaType.APPLICATION_JSON));
-  }
+        MultivaluedMap<String, Object> headerMap = new MultivaluedHashMap<>(headers);
+        return client
+                .target(uri)
+                .request(MediaType.APPLICATION_JSON)
+                .headers(headerMap)
+                .post(Entity.entity(requestObject.toString(), MediaType.APPLICATION_JSON));
+    }
 
-  public Response makePostRequest(String uri, Object requestObject) {
-    return client
-        .target(uri)
-        .request(MediaType.APPLICATION_JSON)
-        .post(Entity.entity(requestObject.toString(), MediaType.APPLICATION_JSON));
-  }
+    public Response makePostRequest(String uri, Object requestObject) {
+        return client
+                .target(uri)
+                .property(ClientProperties.READ_TIMEOUT, 5000)
+                .property(ClientProperties.CONNECT_TIMEOUT, 5000)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(requestObject.toString(), MediaType.APPLICATION_JSON));
+    }
 
-  public String makePostRequestAndGetData(String uri, Object requestObject,
-      HashMap<String, String> headers) {
-    return makePostRequest(uri, requestObject, headers).readEntity(String.class);
-  }
+    public String makePostRequestAndGetData(String uri, Object requestObject,
+                                            HashMap<String, String> headers) {
+        return makePostRequest(uri, requestObject, headers).readEntity(String.class);
+    }
 
 }
