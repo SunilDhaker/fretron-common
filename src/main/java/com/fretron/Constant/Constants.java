9094a631ea663afdfd195247cdf69cd0efb40cb4
rohitpalod <palodrht@gmail.com>
2018-10-06 13:23:46 +0530

    Constant for location trail collection

--- src/main/java/com/fretron/Constant/Constants.java
+++ src/main/java/com/fretron/Constant/Constants.java
@@ -294,6 +294,7 @@ public class Constants {
     public static final String FRETRON_GOD_FO_AGENT = "fretron.god.fo.agent";
     public static final String SHIPMENT_COLLECTION_NAME = "shipment.collection.name";
     public static final String CONSIGNMENT_COLLECTION_NAME = "consignment.collection.name";
+    public static final String LOCATION_TRAIL_COLLECTION_NAME = "location.trail.collection.name";
     public static final String SHIPMENT_TRACKING_REQUEST_COLLECTION_NAME = "shipment.tracking.request.collection.name";
     public static final String TRUSTED_ORG_COLLECTION_NAME = "shipment.trustedorg.collection.name";
     public static final String SHIPMENT_DB_NAME = "shipment.db.name";
