4a0bc49a1ffc8045420058444ee6951b49d7fba5
Mohit <sahumohit00786@gmail.com>
2020-05-26 17:26:46 +0530

    add clean contract and line item extension

---
+++ src/main/java/com/fretron/Extensions/ContractExtension.kt
@@ -0,0 +1,46 @@
+package com.fretron.Extensions
+
+import com.fretron.Model.Contract
+import com.fretron.Model.ContractLineItem
+import com.fretron.Model.Materials
+
+
+fun Contract.clean() {
+    if (this.getSalesOffice() != null) {
+        this.getSalesOffice().clean()
+    }
+    if (this.getCustomer() != null) {
+        this.getCustomer().clean()
+    }
+
+    this.getAllowedBillToParties()?.forEach { it.clean() }
+    this.getAllowedConsignee()?.forEach { it.clean() }
+    this.getAllowedConsigner()?.forEach { it.clean() }
+    this.getAllowedBillingBranches()?.forEach { it.deepClean() }
+    this.getAllowedConsignmentBranches()?.forEach { it.deepClean() }
+    this.getAllowedBillingBranches()?.forEach { it.deepClean() }
+    this.getAllowedPayers()?.forEach { it.clean() }
+    this.getAllowedPaymentCollectionBranches()?.forEach { it.deepClean() }
+    this.getAllowedOrderBranches()?.forEach { it.deepClean() }
+}
+
+
+
+fun ContractLineItem.clean(){
+    this.getContractBranch()?.clean()
+    this.getContractOwner()?.clean()
+    this.getConsignee()?.clean()
+    this.getConsignor()?.clean()
+    this.getConsignmentBranch()?.clean()
+    this.getBillToParty()?.clean()
+    this.getBillingBranch()?.clean()
+    this.getDeliveryBranch()?.clean()
+    this.getPayer()?.clean()
+    this.getMaterial()?.clean()
+    this.getMaterialService()?.clean()
+}
+
+
+fun Materials.clean(){
+    this.setUpdates(null)
+}
