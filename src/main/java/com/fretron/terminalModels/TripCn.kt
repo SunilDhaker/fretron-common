ce1c428f450f2661c4331ef8998de4814d5043f8
Mohit Sahu <mohit.sahu@fretron.com>
2022-05-19 16:51:46 +0530

    update: TripCN

--- src/main/java/com/fretron/terminalModels/TripCn.kt
+++ src/main/java/com/fretron/terminalModels/TripCn.kt
@@ -9,27 +9,41 @@ data class TripCn(
     var orgId: String?,
     var externalId: String?,
     var consignmentNumber: String?,
-    var consignee: ResourceInfo?,
-    var consigner: ResourceInfo?,
+    var consignee: LiteBusinessPartner?,
+    var consigner: LiteBusinessPartner?,
     var originTerminal: ResourceInfo?,
     var destinationTerminal: ResourceInfo?,
     var currentTerminal: ResourceInfo?,
     var tripInfo: ResourceInfo?,
     var containerInfo: List<ResourceInfo>?,
     var orderMappings: List<SalesOrderMapping>?,
-    var assosiatedShipments: List<String>?,
+    var associatedShipments: List<String>?,
     var orderNumber: String?,
     var lineItems: List<ConsignmentLineItem>?,
     var currentAssignedResource: ResourceInfo?,
     var nextPossibleActions: List<String>?,
-    var containerizationType: String?
+    var containerizationType: String?,
+    var consignmentDate: Long?
 ) {
 
     constructor() : this(
         null, null, null, null, null, null,
         null, null, null, null, null, null,
-        null, null, null, null, null, null
+        null, null, null, null, null, null,
+        null
     )
+
+    override fun toString(): String {
+        return GsonBuilder().serializeNulls().create().toJson(this)
+    }
+}
+
+
+data class LiteBusinessPartner(
+    var uuid: String?, var externalId: String?, var name: String?, var placeId: String?,
+    var placeExternalId: String?, var placeName: String?, var address: String?
+) {
+    constructor() : this(null, null, null, null, null, null, null)
 
     override fun toString(): String {
         return GsonBuilder().serializeNulls().create().toJson(this)
