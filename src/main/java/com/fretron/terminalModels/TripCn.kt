cc1007121f8fbe4645954b6f54e184b5a4653633
Mohit Sahu <mohit.sahu@fretron.com>
2022-08-23 13:06:37 +0530

    update(TripCn): add terminal arrival and departure time in tripCN

--- src/main/java/com/fretron/terminalModels/TripCn.kt
+++ src/main/java/com/fretron/terminalModels/TripCn.kt
@@ -4,20 +4,22 @@ import com.fretron.Model.ConsignmentLineItem
 import com.fretron.Model.SalesOrderMapping
 import com.fretron.Model.StandardMeasurement
 import com.google.gson.GsonBuilder
+import javax.ws.rs.NotAllowedException
 
 data class TripCn(
     var uuid: String?,
-    var consignmentId : String?,
+    var consignmentId: String?,
     var orgId: String?,
     var externalId: String?,
     var tripId: String?,
+    var tripNumber: String?,
     var consignmentNumber: String?,
     var consignee: LiteBusinessPartner?,
     var consigner: LiteBusinessPartner?,
-    var customer : LiteBusinessPartner?,
-    var originTerminal: ResourceInfo?,
-    var destinationTerminal: ResourceInfo?,
-    var currentTerminal: ResourceInfo?,
+    var customer: LiteBusinessPartner?,
+    var originTerminal: TerminalInfo?,
+    var destinationTerminal: TerminalInfo?,
+    var currentTerminal: TerminalInfo?,
     var containerInfo: List<ResourceInfo>?,
     var orderMappings: MutableList<SalesOrderMapping> = mutableListOf(),
     var plannedMapping: MutableList<SalesOrderMapping> = mutableListOf(),
@@ -33,16 +35,15 @@ data class TripCn(
     var scope: String?,
     var urgencyType: String?,
     var status: String?,
-    var targetDeliveryDate : Long?
+    var targetDeliveryDate: Long?
 ) {
 
     constructor() : this(
-        null , null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null,
         null, null, null, null, null, null,
         mutableListOf(), mutableListOf(), null, null, null, null,
-        null, null, null, null, null, null ,null , null , null
+        null, null, null, null, null, null, null, null, null
     )
-
 
 
     override fun toString(): String {
@@ -63,6 +64,33 @@ data class LiteBusinessPartner(
 }
 
 
-enum class TripCnStatus{
-    Planned , OnTrip ,CustomerHandoff , Completed
+enum class TripCnStatus {
+    Planned, OnTrip, CustomerHandoff, Completed
+}
+
+data class TerminalInfo(
+    var resourceId: String?,
+    var resourceType: String?,
+    var resourceIdentifier: String?,
+    var resourceExtId: String?,
+    var arrivalTime: Long?,
+    var departureTime: Long?
+) {
+    constructor() : this(null, null, null, null, null, null)
+
+    override fun toString(): String {
+        return GsonBuilder().serializeNulls().create().toJson(this)
+    }
+
+    @Throws(NotAllowedException::class)
+    fun validateOrThrow() {
+        if (resourceId.isNullOrBlank() || resourceType.isNullOrBlank() || resourceIdentifier.isNullOrBlank()) {
+            throw NotAllowedException("Bad Request, Invalid resource info.")
+        }
+    }
+
+    fun isValid(): Boolean {
+        return !(resourceId.isNullOrBlank() || resourceType.isNullOrBlank() || resourceIdentifier.isNullOrBlank())
+    }
+
 }
